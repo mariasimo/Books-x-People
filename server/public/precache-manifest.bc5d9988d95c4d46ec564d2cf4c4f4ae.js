b7bd6c40844a211a04fc4261fd2dfbae4acf3f35
María Simó <maria@coponstudio.es>
2020-03-22 21:52:37 +0100

    changed manifest

---
+++ server/public/precache-manifest.bc5d9988d95c4d46ec564d2cf4c4f4ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2720d166801a6e99a34759cd7a53ddd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4d7af9e7347eec50e3",
+    "url": "/static/css/main.d8865039.chunk.css"
+  },
+  {
+    "revision": "d39a5d34f879bf3e9336",
+    "url": "/static/js/2.7a27f2be.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/static/js/2.7a27f2be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e4d7af9e7347eec50e3",
+    "url": "/static/js/main.c1cb6133.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
