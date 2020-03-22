807756edc22e586c86d1846c50da6dba165ea768
María Simó <maria@coponstudio.es>
2020-03-22 01:54:30 +0100

    Basic picked book funcionality

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Loading from '../Loading'
 import { Link } from 'react-router-dom';
 import ErrorMessage from '../ErrorMessage'
@@ -12,13 +12,15 @@ import {useQuery} from '@apollo/react-hooks';
 // * We import it from:
 import { GET_BOOKS } from '../../queries' 
 
-const BookItem = ({name, author, recommendedBy, width, height, id}) => {  
+const BookItem = (props) => {  
 
+    const {name, width, author, recommendedBy, height, id, getSelected, pickedBook} = props
     const factor = (name.length > 25 || author.length > 25) ? .6 : .8;
     const fontSize = width.replace('em', '')* factor + 'em';
 
+    console.log(pickedBook, id)
     return (
-        <li>
+        <li onClick={getSelected} className={(pickedBook === id) ? 'picked-book' : ""}>
             <Link to={`/libro/${recommendedBy}/${id}`}>
                 <div className="vertical-text" style={{width:width, height: height, fontSize: fontSize}}>
                     <span className="book-title">{name}</span>
@@ -30,6 +32,7 @@ const BookItem = ({name, author, recommendedBy, width, height, id}) => {
 }
     
 const BookList = () => {
+    const [selected, setSelected] = useState(null)
     
     // Then we have to bind it to our component 
     // For that, we use useQuey
@@ -44,10 +47,12 @@ const BookList = () => {
     return (
         <ul className="book-list">
             {books.map( book => (   
-                <BookItem key={book.id} {...book}/>)
+                <BookItem key={book.id} {...book} pickedBook={selected} getSelected={() => setSelected(book.id)}/>)
             )}
         </ul>
     )
 }
 
 export default BookList;
+
+
