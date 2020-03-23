b3c44f2420d9467119cc4ba1b4d6de4b6ad72e67
María Simó <maria@coponstudio.es>
2020-03-23 12:18:36 +0100

    Responsive header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,11 +5,19 @@ import './styles.scss'
 const Header = () => {
     return (
         <header>
-            <img src={`${process.env.REACT_APP_URL}/book-icon.svg`} alt="Book icon"/>
             <Link to="/">
-                <p className="title t3 vertical-text">Books x People</p>
+            <div className="desktop">
+                <img src={`${process.env.REACT_APP_URL}/book-icon.svg`} alt="Book icon"/>
+                    <p className="title t3 vertical-text">Books x People</p>
+                <img src={`${process.env.REACT_APP_URL}/people-icon.svg`} alt="People icon"/>
+            </div>
+            
+            <div className="mobile">
+                <img src={`${process.env.REACT_APP_URL}/acronym.svg`} alt="People icon"/>
+                <img src={`${process.env.REACT_APP_URL}/pictogram.svg`} alt="People icon"/>
+            </div>
+
             </Link>
-            <img src={`${process.env.REACT_APP_URL}/people-icon.svg`} alt="People icon"/>
         </header>
     )
 }
