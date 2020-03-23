eb7b5b18af5f0b5e297efb31327d4cb8dfda7894
María Simó <maria@coponstudio.es>
2020-03-23 13:25:25 +0100

    Changes to env.prod

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,8 +8,8 @@ const mailTemplate = require('../mailTemplate');
 var transport = {
   host: 'smtp.gmail.com',
   auth: {
-    user:"booksxpeople@gmail.com",
-    pass:"26Pamplinas!"
+    user:process.env.MAILUSER,
+    pass:process.env.MAILPASS
   }
 }
 
