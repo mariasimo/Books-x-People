77093ecea080fbe69ee8817d11e7fc9c82450c03
María Simó <maria@coponstudio.es>
2020-03-24 18:08:48 +0100

    Removed meta tafs

---
+++ server/public/precache-manifest.e7bf08c8d353be8965a1e99216ea9bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79242cf0208e896e4a5759dca7a482de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0396599f5e4c1a172bf8",
+    "url": "/static/css/main.b643d19f.chunk.css"
+  },
+  {
+    "revision": "1d1fa3ec340d9456ed00",
+    "url": "/static/js/2.8ad13af6.chunk.js"
+  },
+  {
+    "revision": "c27e167d6e2cc1d9eddf7fde2be56a6c",
+    "url": "/static/js/2.8ad13af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0396599f5e4c1a172bf8",
+    "url": "/static/js/main.278ead33.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
